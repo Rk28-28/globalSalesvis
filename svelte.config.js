97e108f897e4af87ceb81da7bc6be0fa8ae501ee
Ritvik Kosuri <kosur005@umn.edu>
2026-01-14 16:47:26 -0600

    X

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,23 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html", // SPA fallback for GitHub Pages
-    }),
-
-    paths: {
-      base: "/globalSalesvis" // replace with your repo name
-    },
-
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter(),
     alias: {
       "@data-types": "src/lib/types",
       "@utils": "src/lib/utils",
-      "@components": "src/lib/components"
-    }
-  }
+      "@components": "src/lib/components",
+    },
+  },
 };
 
 export default config;
