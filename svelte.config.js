da2bad8fadc0e681dc3efb9d376b151369ad9643
Ritvik Kosuri <kosur005@umn.edu>
2026-01-14 16:03:01 -0600

    Fix SvelteKit static config for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html", // REQUIRED for GitHub Pages
+    }),
+
+    paths: {
+      base: "/globalSalesvis", // REQUIRED for repo-based Pages
+    },
+
     alias: {
       "@data-types": "src/lib/types",
       "@utils": "src/lib/utils",
